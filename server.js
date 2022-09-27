f82f2f6b4d20e2676d386115a432ac5598cb0f35
Choogi <drewtex@gmail.com>
2022-09-27 18:20:14 -0500

    herokufix

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-const PORT = 3001 || process.env.PORT ;
+const PORT = process.env.PORT || 3001 ;
 
 
 app.listen(PORT, () => {
